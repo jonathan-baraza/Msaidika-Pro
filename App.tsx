5c052ba7738f8795f91e5f1186af0acfeb606138
jonathan_baraza <jbaraza46@gmail.com>
2023-07-10 10:15:22 +0300

    feat: expo gradient

--- App.tsx
+++ App.tsx
@@ -1,21 +1,44 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Welcome from "./screens/Welcome";
+import Login from "./screens/auth/Login";
+import Register from "./screens/auth/Register";
+import { useCallback } from "react";
+
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text className="">Open up App.tsx to start working on your app!</Text>
+  const [isLoaded] = useFonts({
+    "mrt-thin": require("./assets/fonts/Montserrat/Montserrat-Thin.ttf"),
+    "mrt-mid": require("./assets/fonts/Montserrat/Montserrat-Medium.ttf"),
+    "mrt-semi": require("./assets/fonts/Montserrat/Montserrat-SemiBold.ttf"),
+    "mrt-bold": require("./assets/fonts/Montserrat/Montserrat-Bold.ttf"),
+    "mrt-xbold": require("./assets/fonts/Montserrat/Montserrat-ExtraBold.ttf"),
+    "rb-thin": require("./assets/fonts/Roboto/Roboto-Thin.ttf"),
+    "rb-mid": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+    "rb-semi": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+    "rb-bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+    "rb-xbold": require("./assets/fonts/Roboto/Roboto-Black.ttf"),
+  });
 
-      <StatusBar style="auto" />
+  const handleOnLayout = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isLoaded]);
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  return (
+    <View className="flex-1" onLayout={handleOnLayout}>
+      {/* <Welcome /> */}
+      {/* <Login /> */}
+      <Register />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
