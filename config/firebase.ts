63523805b9c4d170b98048936f89f242cc9a747d
jonathan_baraza <jbaraza46@gmail.com>
2023-07-13 23:31:33 +0300

    initial firebase configs

---
+++ config/firebase.ts
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCDrojVGwhUmoOCTpsjreWfyElc90uYOLY",
+//   authDomain: "msaidika-b1994.firebaseapp.com",
+//   projectId: "msaidika-b1994",
+//   storageBucket: "msaidika-b1994.appspot.com",
+//   messagingSenderId: "444579994636",
+//   appId: "1:444579994636:web:3811e8723524abf1e0e229",
+//   measurementId: "G-1GP9SJR8HS",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
