61c7f3f2a17f93f75d9e868a815ca83e4267d8d9
jonathan_baraza <jbaraza46@gmail.com>
2023-07-13 23:50:38 +0300

    fix: plugins inclusion

--- babel.config.js
+++ babel.config.js
@@ -5,17 +5,19 @@ module.exports = function (api) {
     plugins: [
       "nativewind/babel",
       "react-native-reanimated/plugin",
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-        blocklist: null,
-        allowlist: null,
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
     ],
   };
 };
