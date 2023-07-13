b38d2c3054704d27e57b70524a1423bd479e2c9f
jonathan_baraza <jbaraza46@gmail.com>
2023-07-13 23:51:02 +0300

    env tests

--- screens/main/Home.tsx
+++ screens/main/Home.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { PROJECT_ID } from "@env";
 
 const Home = () => {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl">Home Screen</Text>
+      <Text className="text-2xl">Home Screen {PROJECT_ID}</Text>
     </View>
   );
 };
