dcc3921f60243e527013d085c9fbe9009421b2c2
jonathan_baraza <jbaraza46@gmail.com>
2023-07-14 14:56:27 +0300

    auth initialization

--- config/firebase.ts
+++ config/firebase.ts
@@ -10,6 +10,7 @@ import {
   APP_ID,
   MEASUREMENT_ID,
 } from "@env";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -29,3 +30,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
