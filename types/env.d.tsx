33b74994299992e41000e6481aa20bc95832b97b
jonathan_baraza <jbaraza46@gmail.com>
2023-07-17 17:07:13 +0300

    env types

--- types/env.d.tsx
+++ types/env.d.tsx
@@ -9,4 +9,5 @@ declare module "@env" {
   export const MEASUREMENT_ID: string;
   export const WEB_CLIENT_ID: string;
   export const WEB_CLIENT_SECRET: string;
+  export const ANDROID_CLIENT_ID: string;
 }
