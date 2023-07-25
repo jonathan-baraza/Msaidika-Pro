ab16933312fbbb022d7ef8f6b4e136faef5e034d
jonathan_baraza <jbaraza46@gmail.com>
2023-07-25 15:04:25 +0300

    job card

--- screens/main/Home.tsx
+++ screens/main/Home.tsx
@@ -14,7 +14,7 @@ const Home = () => {
   const navigation: any = useNavigation();
   return (
     <View className="flex-1 bg-white" style={styles.container}>
-      <View className="border-b border-gray-200 p-0 m-0 relative mb-3 flex flex-row  items-center px-4">
+      <View className="border-b border-gray-200 p-0 m-0 relative flex flex-row  items-center px-4">
         <Pressable className="" onPress={() => navigation.openDrawer()}>
           <AntDesign name="menu-fold" size={20} color="black" />
         </Pressable>
@@ -23,6 +23,22 @@ const Home = () => {
           resizeMode="center"
           source={require("../../assets/logo2.png")}
         />
+      </View>
+      <View className="flex-1 bg-[#f2ffff]">
+        <View className="w-1/2 p-3">
+          <View className="bg-white rounded-lg flex  shadow shadow-xl shadow-black">
+            <Image
+              className="w-full h-[100px] "
+              resizeMode="cover"
+              source={{
+                uri: "https://img.freepik.com/free-photo/hiring-concept-with-empty-chair_23-2149519862.jpg?size=626&ext=jpg&ga=GA1.1.1480004811.1676452419&semt=sph",
+              }}
+            />
+            <View className="p-3 flex justify-center">
+              <Text className="font-bold text-center">Job Opportunities</Text>
+            </View>
+          </View>
+        </View>
       </View>
     </View>
   );
