503a59c895f5777610c07ee227228eceb9e4eb2a
jonathan_baraza <jbaraza46@gmail.com>
2023-07-25 15:45:39 +0300

    sp application screen init

---
+++ screens/main/SPApplication.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SPApplication = () => {
+  return (
+    <View>
+      <Text>SPApplication</Text>
+    </View>
+  );
+};
+
+export default SPApplication;
