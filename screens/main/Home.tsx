e8474580a06c33ccc65d573539e2d96e86534420
jonathan_baraza <jbaraza46@gmail.com>
2023-08-01 11:43:36 +0300

    messages icon

--- screens/main/Home.tsx
+++ screens/main/Home.tsx
@@ -6,7 +6,7 @@ import {
   StyleSheet,
   StatusBar,
 } from "react-native";
-import { Ionicons, AntDesign } from "@expo/vector-icons";
+import { Ionicons, AntDesign, FontAwesome5 } from "@expo/vector-icons";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import Toast from "react-native-root-toast";
@@ -27,15 +27,20 @@ const Home = () => {
 
   return (
     <View className="flex-1 bg-white" style={styles.container}>
-      <View className="border-b border-gray-200 p-0 m-0 relative flex flex-row  items-center px-4">
-        <Pressable className="" onPress={() => navigation.openDrawer()}>
-          <AntDesign name="menu-fold" size={20} color="black" />
-        </Pressable>
-        <Image
-          className="w-[200px] h-[80px] m-0 -ml-4 -z-10"
-          resizeMode="center"
-          source={require("../../assets/logo2.png")}
-        />
+      <View className="border-b border-gray-200 p-0 m-0 relative flex flex-row justify-between  items-center px-4">
+        <View className="flex flex-row items-center">
+          <Pressable className="" onPress={() => navigation.openDrawer()}>
+            <AntDesign name="menu-fold" size={20} color="black" />
+          </Pressable>
+          <Image
+            className="w-[200px] h-[80px] m-0 -ml-4 -z-10"
+            resizeMode="center"
+            source={require("../../assets/logo2.png")}
+          />
+        </View>
+        <View className="pr-2">
+          <FontAwesome5 name="facebook-messenger" size={24} color="#007acc" />
+        </View>
       </View>
 
       <ScrollView
