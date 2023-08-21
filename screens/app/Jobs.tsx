c020d5c53b7601058bade1f59a4af6121e8ad313
jonathan_baraza <jbaraza46@gmail.com>
2023-08-21 14:13:40 +0300

    jobs page init

--- screens/app/Jobs.tsx
+++ screens/app/Jobs.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MenuCover from '../../layouts/MenuCover'
 
 const Jobs = () => {
   return (
-    <View>
-      <Text>Jobs</Text>
-    </View>
-  )
+    <MenuCover>
+      <View>
+        <Text>Jobs</Text>
+      </View>
+    </MenuCover>
+  );
 }
 
 export default Jobs
