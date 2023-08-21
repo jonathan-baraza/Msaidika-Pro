111c25c0258970dcfee6212bdd57e7adb58b8b30
jonathan_baraza <jbaraza46@gmail.com>
2023-08-21 14:21:43 +0300

    route name indicator

--- layouts/MenuCover.tsx
+++ layouts/MenuCover.tsx
@@ -1,15 +1,20 @@
 import { View, Text, StatusBar, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import { Ionicons, AntDesign, FontAwesome5 } from "@expo/vector-icons";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation,useRoute } from "@react-navigation/native";
 
 const MenuCover = ({ children }: { children: any }) => {
   const navigation: any = useNavigation();
+  const route:any=useRoute();
   return (
     <View className="flex-1 m-0 p-0 " style={styles.container}>
-      <View className="py-2 px-4">
+      <View className="py-2 px-4 flex items-center justify-between flex-row bg-white">
         <Pressable className="" onPress={() => navigation.openDrawer()}>
           <AntDesign name="menu-fold" size={20} color="black" />
+        </Pressable>
+        <Text className="semi-bold text-base">{route.name}</Text>
+        <Pressable className="" onPress={() => navigation.pop()}>
+          <AntDesign name="back" size={24} color="black" />
         </Pressable>
       </View>
       {children}
