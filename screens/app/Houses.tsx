74c5947c2ef86a917aa21d5666aee7878ef10ffb
jonathan_baraza <jbaraza46@gmail.com>
2023-08-21 13:46:08 +0300

    app screens

---
+++ screens/app/Houses.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Houses = () => {
+  return (
+    <View>
+      <Text>Houses</Text>
+    </View>
+  )
+}
+
+export default Houses
---
+++ screens/app/Transport.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Transport = () => {
+  return (
+    <View>
+      <Text>Transport</Text>
+    </View>
+  )
+}
+
+export default Transport
